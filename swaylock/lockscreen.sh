580f8c3582758e8827345791cea8914b0772729f
Phobia <137733026+PhobiaGH@users.noreply.github.com>
2024-07-06 21:00:36 -0400

    Added animations to waybar, improved conf files
    Added animations to waybar on hover. Cleaned up the Hyprland.conf file. Improved some of the color choices in the all around style.

--- swaylock/lockscreen.sh
+++ swaylock/lockscreen.sh
@@ -2,7 +2,7 @@
 
 if [ -f "/usr/bin/swayidle" ]; then
 	echo "swayidle is installed."
-	swayidle -w timeout 500 'swaylock -f' #timeout 550 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms oni' # Uncomment "timeout 550" if on bare bones
+	swayidle -w timeout 1800 'swaylock -f' timeout 1850 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' # Uncomment "timeout 550" if on bare bones
 else
 	echo "swayidle is not installed."
 fi;
